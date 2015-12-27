81d95aa2232a0bfa2c0787a87c0770bc6055e495
Jefferson Uchôa Ponte <jefponte@unilab.edu.br>
2015-12-27 02:38:30 -0300

    Controller sendo formados.

--- src/br/com/enviador/controller/ClienteController.java
+++ src/br/com/enviador/controller/ClienteController.java
@@ -1,7 +1,42 @@
 package br.com.enviador.controller;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import br.com.enviador.model.Cliente;
+
 public class ClienteController {
 
+
+	private Cliente cliente;
+	
+	public void tentandoConexao(){
+	
+		
+		/*
+		 * Ao conectar iremos enviar alguns dados necessários 
+		 * para identificação do cliente. Nome da máquina. 
+		 * Depois vamos processar mensagens vindas do servidor pra 
+		 * executar funções de controle da máquina do cliente. 
+		 * 
+		 */
+		
+		this.cliente = new Cliente();
+		Socket conexao;
+		try {
+			conexao = new Socket("localhost", 37389);
+			this.cliente.setConxao(conexao);
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	
 	
 }
--- src/br/com/enviador/controller/ServidorController.java
+++ src/br/com/enviador/controller/ServidorController.java
@@ -1,14 +1,20 @@
 package br.com.enviador.controller;
 
+import java.awt.EventQueue;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import br.com.enviador.model.Cliente;
+
 
 public class ServidorController {
 
 	public ArrayList<Cliente>listaDeClientes;
-	
-
+	public ServerSocket serverSocket;
 	
 	public ServidorController(){
 		
@@ -16,14 +22,75 @@ public class ServidorController {
 	
 	
 	public void iniciaServico(){
-	
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				
+				System.out.println("Servidor Iniciando...");
+				int porta = 37389;
+				System.out.println("Abrindo conexão na porta "+porta);
+				try {
+					
+					serverSocket = new ServerSocket(porta, 10);
+					System.out.println("Seridor Iniciado, aguardando conexões. ");
+					Socket socketCliente;
+					while(true){
+						socketCliente = serverSocket.accept();
+						System.out.println("Nova conexão. Vamos processa-la.");
+						processandoConexao(socketCliente);
+					}
+					
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
 		
 		//Inicia servidor 
 		//Espera conexões. 
 
 		
 	}
+	public void processandoConexao(final Socket socketCliente){
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				Cliente cliente = new Cliente();
+				cliente.setConxao(socketCliente);
+				listaDeClientes.add(cliente);
+				try {
+					ObjectOutputStream saida = new ObjectOutputStream(socketCliente.getOutputStream());
+					saida.flush();
+					saida.writeBytes("Teste");
+					
+					System.out.println("Enviei algo");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+		});
+		
+	}
+	
 	public void iniciaAdministrador(){
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				System.out.println("Administrador Iniciando...");
+				
+				Scanner leitor = new Scanner(System.in);
+				String comando = "";
+				do{
+					System.out.println("Digite um comando ao servidor: ");
+					comando = leitor.nextLine();
+					System.out.println("Você digitou: "+comando);
+					
+				}while(!comando.equals("encerrar"));
+				System.out.println("Encerrando servidor.");
+				System.exit(0);
+			}
+		});
 		//Digite um comando para o servidor
 		
 	}
